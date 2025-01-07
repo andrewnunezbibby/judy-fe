393fe927dbe4418a94ce48f537905942598203bb
Andrew Nunez-Bibby <andrewbibby@Andrews-MacBook-Pro.local>
2025-01-07 17:04:36 -0500

    style chat list and chat bubbles

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { ManifestV3Export, crx } from '@crxjs/vite-plugin';
 
+import manifestJson from './manifest.json';
+
+const manifest = manifestJson as ManifestV3Export;
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tsconfigPaths(), crx({ manifest })],
+});
